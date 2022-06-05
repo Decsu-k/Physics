7acd41abf16ed6dca2c51b31dd0e734491eb3b9d
Decsu-k <82495296+Decsu-k@users.noreply.github.com>
2022-06-05 09:53:31 +0300

    Вывод координат в консоль

--- Physics/Assets/Scripts/PlayerMove.cs
+++ Physics/Assets/Scripts/PlayerMove.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class PlayerMove : MonoBehaviour
 {
+    public class Position
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+        public float z { get; set; }
+        public DateTime Date { get; set; }
+    }
     public Transform player;
+    public List<Position> positions = new List<Position>();
     void Start()
     {
         player.position = new Vector3(0, 2, 0);
@@ -21,5 +30,22 @@ public class PlayerMove : MonoBehaviour
     void OnGUI()
     {
         GUILayout.Label(transform.position.ToString());
+
+        positions.Add(new Position()
+        {
+            Date = DateTime.Now,
+            x = transform.position.x,
+            y = transform.position.y,
+            z = transform.position.z,
+        });
+
+        var lastpos = positions[positions.Count - 2];
+
+        positions.Remove(lastpos);
+
+        print(positions[positions.Count - 1].x);
+        print(positions[positions.Count - 1].y);
+        print(positions[positions.Count - 1].z);
+        print(positions[positions.Count - 1].Date);
     }
 }
