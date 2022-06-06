c9c757688e65d5c1905bf4f9bfc834ab39a9424e
Decsu-k <82495296+Decsu-k@users.noreply.github.com>
2022-06-06 22:21:01 +0300

    Разбор уравнения
    Разбор уравнения для силы трения

--- Physics/Assets/Scripts/GroundMove.cs
+++ Physics/Assets/Scripts/GroundMove.cs
@@ -6,11 +6,13 @@ using System;
 public class GroundMove : MonoBehaviour
 {
     public float speed = 2f;
-    public float normalSpeed;
     public KeyCode moveRight;
     public KeyCode moveLeft;
     public Transform ground;
 
+    void Start()
+    {
+    }
     void Update()
     {
         if (Input.GetKey(moveRight))
@@ -25,41 +27,32 @@ public class GroundMove : MonoBehaviour
             if (Input.GetKey(KeyCode.LeftArrow))
                 transform.Rotate(Vector3.forward, m_y, Space.Self);
         }
+       
+        if (Pressed) Debug.Log("Кнопка нажата");
     }
-    public void OnRight()
-    {
-        float m_y = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.RightArrow))
-            transform.Rotate(new Vector3(), m_y, Space.Self);
-    }
-
-    public void OnLeft()
-    {
-        if (Input.GetKey(moveLeft))
-        {
-            float m_y = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
-            if (Input.GetKey(KeyCode.LeftArrow))
-                transform.Rotate(new Vector3(), m_y, Space.Self);
-        }
-    }
-
-    public void Stop()
-    {
-        transform.Rotate(Vector3.forward, Space.Self);
-    }
-
     void OnGUI()
     {
-        double n = 0.2; //Коэффициент трения
-        int mas = 1;
-        double g = 9.8;
+        double n = 0.25; //Коэффициент трения (ню) (коэффициет пропорциональности)
+        int mas = 1;  //масса объекта
+        double g = 9.8; //(ускорение свободного падения)  ...Сила реакции опоры (сила упругости) (N - нормальная сила (перпендикулярно поверхности земли).(Н))
         Vector3 rot = ground.transform.eulerAngles;
 
         double ugl = rot.z;
 
-        GUILayout.Label(Math.Abs(Math.Round(n * mas * g * Math.Cos(ugl), 2)).ToString() + " H под углом " + ugl); // H - ньютоны 
-        GUILayout.Label(Math.Abs(Math.Round(n * mas * g, 2)).ToString() + " H по горизонтальной плоскости");
+        GUILayout.Label(Math.Abs(Math.Round(n * mas * g * Math.Cos(ugl), 2)).ToString() + " H под углом " + Math.Round(ugl,2)); // H - ньютоны (mg - сила тяжести, (mas*g)) (Fтр = uN = umgcos(а))
+        
+        GUILayout.Label(Math.Abs(Math.Round(n * mas * g, 2)).ToString() + " H по горизонтальной плоскости"); //сила трения (скольжения)(качения) покоя + (F = uN, где N = mg, u = ню)
     }
 
+    bool Pressed = false;
+    public void onDown()
+    {
+        Pressed = true;
+    }
+
+    public void onUp()
+    {
+        Pressed = false;
+    }
 }
 
